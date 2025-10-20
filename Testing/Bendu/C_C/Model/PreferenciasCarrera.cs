587062545a90ff4c7936edeba0b1afee757a9f2a
BenduOlo253 <jmanuelrb1@outlook.com>
2025-10-19 20:38:11 -0600

    .

---
+++ Testing/Bendu/C_C/Model/PreferenciasCarrera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_C.Model
+{
+     public class PreferenciasCarrera
+    {
+        public int IdPreferencia { get; set; }
+        public int IdCarrera { get; set; }
+    }
+}
